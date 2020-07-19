0d9c31e792d8d7f0c728a4f61c6e0d86c35e6f96
biasdamiasa <biasd30@gmail.com>
2020-07-19 22:58:20 +0700

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-form @add:employee="addEmployee" />
+    <employee-table v-bind:employees="employees" @delete:employee="deleteEmployee" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable'
+import EmployeeForm from './components/EmployeeForm'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Bias Damiasa',
+          email: 'biasd30@gmail.com'
+        },
+        {
+          id: 2,
+          name: 'Stephanie H. Unger',
+          email: 'stephanie@jourrapide.com'
+        },
+        {
+          id: 3,
+          name: 'James T. McGinnis',
+          email: 'james@armyspy.com'
+        },
+      ]
+    }
+  },
+  methods: {
+    addEmployee(emp) {
+      const lastId = 
+            this.employees.length > 0 ? 
+                this.employees[this.employees.length-1].id : 0;
+      const id = lastId + 1;
+      const newEmployee = {...emp, id};
+      this.employees = [...this.employees, newEmployee];
+    },
+    deleteEmployee(id) {
+      this.employees = this.employees.filter(
+        employee => employee.id !== id
+      )
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .email-container {
+    max-width: 680px;
+  }
 </style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div id="employee-table">
+        <p v-if="employees.length < 1" class="empty-table">
+            No employees
+        </p>
+        <table v-else>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email </th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+          <td>
+              <button>Edit</button>
+              <button @click="$emit('delete:employee', employee.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+        </table>
+    </div>
+
+</template>
+
+<style scoped>
+    button {
+        margin: 0 0.5rem 0 0;
+    }
+</style>
+
+<script>
+export default {
+ name: 'employee-table',
+ props: {
+     employees: Array
+ },
+}
+</script>
